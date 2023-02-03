a6f13f1b0f433555d56f1766c9581a60cf5c3c28
Zahid <zahid67635gmail.com>
2023-02-03 21:45:42 +0600

    specific property details api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 app.use(cors());
@@ -22,6 +22,12 @@ async function run() {
             const properties = await allPropertyCollection.find(query).toArray();
             res.send(properties);
         });
+        app.get('/allProperties/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await allPropertyCollection.findOne(query);
+            res.send(result);
+        })
 
         app.put('/users', async (req, res) => {
             const user = req.body;
