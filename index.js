275b7edf08a01b7ddfab09d407233fdf5bf2079d
Zahid <zahid67635gmail.com>
2023-03-05 22:21:55 +0600

    host req api created

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gtp2h2a.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -18,21 +17,40 @@ async function run() {
         const allPropertyCollection = client.db('Rent_Roll').collection('all_properties');
         const usersCollection = client.db('Rent_Roll').collection('users');
         const bookingsCollection = client.db('Rent_Roll').collection('bookingsCollection');
+        const hostReqCollection = client.db('Rent_Roll').collection('hostReqCollection');
         app.get('/allProperties', async (req, res) => {
             const query = {};
             const properties = await allPropertyCollection.find(query).toArray();
             res.send(properties);
         });
+        app.post('/allProperties', async (req, res) => {
+            const data = req.body
+            const properties = await allPropertyCollection.insertOne(data)
+            res.send(properties);
+        });
         app.get('/allProperties/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectId(id) };
+            const query = { id: id };
             const result = await allPropertyCollection.findOne(query);
             res.send(result);
         })
+        app.get('/searchProperties/:location', async (req, res) => {
+            const Location = req.params.location;
+            const query = { Location: Location };
+            const result = await allPropertyCollection.find(query).toArray();
+            res.send(result);
+        })
 
-        app.put('/users', async (req, res) => {
-            const user = req.body;
-            const result = await usersCollection.insertOne(user);
+        app.put('/users/:email', async (req, res) => {
+            const email = req.params.email
+            const userDetails = req.body;
+            console.log(userDetails, email);
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: userDetails,
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
             res.send(result);
         })
 
@@ -43,6 +61,11 @@ async function run() {
             res.send(user)
         })
 
+        app.put('/hostRequests', async (req, res) => {
+            const data = req.body
+            const result = await hostReqCollection.insertOne(data);
+            res.send(result);
+        })
         app.post('/bookings', async (req, res) => {
             const data = req.body
             const result = await bookingsCollection.insertOne(data);
@@ -54,6 +77,13 @@ async function run() {
             const result = await bookingsCollection.findOne(query)
             res.send(result);
         })
+        app.get('/bookings/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const result = await bookingsCollection.find(query).toArray();
+            res.send(result);
+        })
+
     }
     finally {
 
